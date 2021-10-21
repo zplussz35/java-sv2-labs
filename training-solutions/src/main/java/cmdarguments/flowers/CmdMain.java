e42beff855a82c6d6628a801dfa47bba7a2d4e2d
zplussz35 <15.zoltan.16@gmail.com>
2021-10-21 22:09:42 +0200

    cmdarguments.flower.CmdMain elkészítése

---
+++ training-solutions/src/main/java/cmdarguments/flowers/CmdMain.java
@@ -0,0 +1,11 @@
+package cmdarguments.flowers;
+
+public class CmdMain {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println((i+1)+". "+args[i]);
+        }
+
+    }
+}
