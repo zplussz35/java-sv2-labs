0e8f54639837fe5760f10a917d178cb55225b7b1
zplussz35 <15.zoltan.16@gmail.com>
2021-10-21 16:59:32 +0200

    javaapi: Upper elkészítése

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,17 @@
+package javaapi;
+
+public class Upper {
+    String text="Hello World!".toUpperCase();
+
+    public static void main(String[] args) {
+        Upper upper = new Upper();
+        System.out.println(upper.text);
+    }
+
+
+
+
+
+
+
+}
