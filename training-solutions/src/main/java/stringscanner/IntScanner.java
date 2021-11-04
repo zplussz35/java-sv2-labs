f39530cfd137c9b8828ea92c7715d28e44d89ddd
zplussz35 <15.zoltan.16@gmail.com>
2021-11-04 13:54:38 +0100

    stringscanner IntScanner elkészítése

---
+++ training-solutions/src/main/java/stringscanner/IntScanner.java
@@ -0,0 +1,33 @@
+package stringscanner;
+
+import java.util.Scanner;
+
+public class IntScanner {
+    public String convertInts(String ints){
+        Scanner sc = new Scanner(ints).useDelimiter(";");
+        StringBuilder sb = new StringBuilder();
+        while(sc.hasNext()){
+            int num= Integer.parseInt(sc.next());
+            if(num>100){
+                sb.append(num);
+                sb.append(",");
+            }
+        }
+        if(sb.length()!=0){
+            sb.deleteCharAt(sb.length()-1);
+        }
+        else{
+            sb.append("Nincs 100-nál nagyobb szám, vagy üres string!");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        IntScanner isc = new IntScanner();
+        String text="43;";
+        System.out.println(isc.convertInts(text));
+    }
+
+
+}
