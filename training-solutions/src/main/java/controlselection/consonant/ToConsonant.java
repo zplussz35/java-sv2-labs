699e1c26a2ae5fbe3dea324f64089e3b5832ea19
zplussz35 <15.zoltan.16@gmail.com>
2021-11-09 11:59:41 +0100

    controlselection ToConsonant elkészítése és week04.md selfcheck update

---
+++ training-solutions/src/main/java/controlselection/consonant/ToConsonant.java
@@ -0,0 +1,34 @@
+package controlselection.consonant;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ToConsonant {
+    private final List<Character> MSH= Arrays.asList('b','c','d','f','g','h','j','k','l','m',
+            'n','p','q','r','s','t','v','w','x','y','z',
+            'B','C','D','F','G','H','J','K','L','M',
+            'N','P','Q','R','S','T','V','W','X','Y','Z');
+    private final List<Character> MGH= Arrays.asList('a','e','i','o','u','A','E','I','O','U');
+    public char toConsonant(char c) {
+        for (char ch: MSH) {
+            if(c==ch){
+                return c;
+            }
+        }
+
+        for (char ch: MGH) {
+            if(c==ch){
+                return (char)(c+1);
+            }
+        }
+        return '0';
+
+    }
+
+    public static void main(String[] args) {
+        ToConsonant con= new ToConsonant();
+
+        System.out.println(con.toConsonant('Z'));
+    }
+}
