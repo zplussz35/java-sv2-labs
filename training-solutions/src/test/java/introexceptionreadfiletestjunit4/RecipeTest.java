ad74972dec053892bdd62b9a52bb7732b7f6045e
zplussz35 <15.zoltan.16@gmail.com>
2021-11-25 18:21:16 +0100

    introexceptionreadfiletestjunit4 RecipeTest elkészítése

---
+++ training-solutions/src/test/java/introexceptionreadfiletestjunit4/RecipeTest.java
@@ -0,0 +1,20 @@
+package introexceptionreadfiletestjunit4;
+
+import org.junit.Test;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class RecipeTest {
+    
+
+    @Test
+    public void addIngredients() {
+        Recipe recipe=new Recipe();
+        recipe.addIngredients(Paths.get("src/test/resources/recipe.txt"));
+        assertEquals(Arrays.asList("liszt", "margarin", "kristálycukor", "tojás", "citrom",
+                "sütőpor", "vanillincukor", "tejföl", "alma", "fahéj"),recipe.getIngredients());
+    }
+}
