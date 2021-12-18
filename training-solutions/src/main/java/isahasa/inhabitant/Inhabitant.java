21e782482aa5bd593aefa9b64f7e1b3878bd0ee8
zplussz35 <15.zoltan.16@gmail.com>
2021-12-18 18:43:25 +0100

    isahasa: inhabitant

---
+++ training-solutions/src/main/java/isahasa/inhabitant/Inhabitant.java
@@ -0,0 +1,18 @@
+package isahasa.inhabitant;
+
+
+public class Inhabitant extends Person {
+    private ParkingPlace parkingPlace;
+
+    public Inhabitant(String name, ParkingPlace parkingPlace) {
+        super(name);
+        this.parkingPlace = parkingPlace;
+    }
+
+    public ParkingPlace getParkingPlace() {
+        return parkingPlace;
+    }
+    public void changeParkingPlace(int number){
+        this.parkingPlace= new ParkingPlace(number);
+    }
+}
