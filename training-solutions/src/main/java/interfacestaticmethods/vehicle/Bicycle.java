e04812543e25cb6df96406126f1c51a6d5bbec8a
zplussz35 <15.zoltan.16@gmail.com>
2021-12-18 15:34:08 +0100

    interfacestaticmethods: vehicle

---
+++ training-solutions/src/main/java/interfacestaticmethods/vehicle/Bicycle.java
@@ -0,0 +1,9 @@
+package interfacestaticmethods.vehicle;
+
+public class Bicycle implements Vehicle{
+    private int numberOfWheels;
+
+    public Bicycle(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+}
---
+++ training-solutions/src/main/java/interfacestaticmethods/vehicle/Vehicle.java
@@ -0,0 +1,16 @@
+package interfacestaticmethods.vehicle;
+
+public interface Vehicle {
+    static Vehicle of(int numberOfWheels){
+        if(numberOfWheels==2){
+            return new Bicycle(numberOfWheels);
+        }
+        else if(numberOfWheels==4){
+            return new Car(numberOfWheels);
+        }
+        else{
+            throw new IllegalArgumentException("This is neither a bicycle nor a car.");
+        }
+    }
+
+}
