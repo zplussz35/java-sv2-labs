0fb46466273b0fe5bf03fe6d49c5ab59524e308a
zplussz35 <15.zoltan.16@gmail.com>
2022-02-10 19:45:28 +0100

    sorting CarDealer elkészítése

---
+++ training-solutions/src/main/java/sorting/CarPriceComparator.java
@@ -0,0 +1,10 @@
+package sorting;
+
+import java.util.Comparator;
+
+public class CarPriceComparator implements Comparator<Car> {
+    @Override
+    public int compare(Car one, Car other) {
+        return Integer.valueOf(one.getPrice()).compareTo(other.getPrice());
+    }
+}
